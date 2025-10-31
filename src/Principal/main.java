127e1b68bdea8a4d1bf65e517a5037b62a76831a
David rodriguez <Davidro08@educastur.es>
2025-10-31 12:09:47 +0100

    bghfh

--- src/Principal/main.java
+++ src/Principal/main.java
@@ -1,6 +1,9 @@
 package Principal;
 
 public class main {
+	public void main() {
+	System.out.println("hola pedro sanchez");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

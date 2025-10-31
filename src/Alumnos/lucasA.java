250ea708400fe9d06b1d56e235d45672e5060730
Lucas Fernandez <lucasfa10@educastur.es>
2025-10-31 12:21:47 +0100

    uwuwded

--- src/Alumnos/lucasA.java
+++ src/Alumnos/lucasA.java
@@ -1,5 +1,8 @@
 package Alumnos;
 
 public class lucasA {
+	public void lucas() {
+		System.out.println("MellamoLucas");
+	}
 
 }
--- src/Principal/main.java
+++ src/Principal/main.java
@@ -1,6 +1,9 @@
 package Principal;
 
 public class main {
+	public void main() {
+		System.out.println("Hola santi abascal");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

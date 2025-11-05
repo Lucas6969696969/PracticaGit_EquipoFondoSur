fbf7ccea87a0881a95e01855dcf0be9cff2b4d54
Lucas Fernandez <lucasfa10@educastur.es>
2025-11-05 13:12:47 +0100

    4353

--- src/Principal/main.java
+++ src/Principal/main.java
@@ -4,6 +4,7 @@ public class main {
 	public void main() {
 		System.out.println("Hola santi abascal");
 		System.out.println("hola pedro sanchez");
+		System.out.println("hola kletus");
 	}
 
 	public static void main(String[] args) {
